80d82089d3c5f2f8904f87373d52b59cc02b091d
gunderwulde <gunderwulde@hotmail.es>
2017-04-25 18:27:36 +0200

    Add files via upload

---
+++ Client/Assets/EntryPoint.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using UnityEngine;
+
+
+using ProtoBuf;
+using ProtoBuf.Meta;
+
+public class EntryPoint : MonoBehaviour {
+	// Use this for initialization
+	void Start () {
+        SerializableMessageBase.Init();
+
+        //Listener();
+        var client = new System.Net.Sockets.TcpClient("127.0.0.1", 16384);
+        System.Net.Sockets.NetworkStream stream = client.GetStream();
+
+        SerializableMessage1 testClass = new SerializableMessage1();
+        testClass.Send(stream);
+
+        stream.Close();
+        client.Close();
+    }
+
+    // Update is called once per frame
+    void Update () {
+    }
+
+}
