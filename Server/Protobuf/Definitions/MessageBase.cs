fdc74a220faa9cbaf3ad66c0330e7b479a456988
gunderwulde <gunderwulde@hotmail.es>
2017-04-26 13:02:34 +0200

    Delete MessageBase.cs

--- Server/Protobuf/Definitions/MessageBase.cs
+++
@@ -1,9 +0,0 @@
-﻿using ProtoBuf;
-
-[ProtoContract]
-[ProtoInclude(2, typeof(MessageTest1))]
-[ProtoInclude(3, typeof(MessageTest2))]
-public class MessageBase {
-    public virtual void Process() { }
-}
-
