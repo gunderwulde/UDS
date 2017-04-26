88a956bb08457295b867e26da7e31aa082b19b5c
gunderwulde <gunderwulde@hotmail.es>
2017-04-26 09:52:32 +0200

    Add files via upload

--- Client/Assets/EntryPoint.cs
+++ Client/Assets/EntryPoint.cs
@@ -7,23 +7,20 @@ using ProtoBuf;
 using ProtoBuf.Meta;
 
 public class EntryPoint : MonoBehaviour {
-	// Use this for initialization
-	void Start () {
-        SerializableMessageBase.Init();
+    Client client;
+    // Use this for initialization
+    void Start () {
+        Serializer.Init();
 
-        //Listener();
-        var client = new System.Net.Sockets.TcpClient("127.0.0.1", 16384);
-        System.Net.Sockets.NetworkStream stream = client.GetStream();
-
-        new SerializableMessage1().Send(stream);
-//        new SerializableMessage2().Send(stream);
-
-        stream.Close();
-        client.Close();
+        var tcpClient = new System.Net.Sockets.TcpClient("127.0.0.1", 16384);
+        client = new Client(tcpClient);
+        client.Send(new MessageTest1());
+        client.Send(new MessageTest2());
     }
 
     // Update is called once per frame
     void Update () {
+        client.Process();
     }
 
 }
