c50b337e1f831a57b25bff511a1d1885f14367b6
gunderwulde <gunderwulde@hotmail.es>
2017-04-26 09:53:55 +0200

    Add files via upload

---
+++ Client/Assets/Protobuf/Definitions/MessageTest2.cs
@@ -0,0 +1,15 @@
+﻿using ProtoBuf;
+
+[ProtoContract]
+public class MessageTest2 : MessageBase { 
+    [ProtoMember(1)]
+    int _data { get; set; }
+    public MessageTest2() {
+        _data = 16384;
+    }
+
+    public override void Process(Client client) {
+        UnityEngine.Debug.Log(">>>> MessageTest2");
+    }
+}
+
