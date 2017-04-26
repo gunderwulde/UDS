0d2e3d9e79e93626f1bcf14e5c26c4be909d997e
Fernando <Fernando@DESKTOP-VDM5AKU>
2017-04-26 12:58:39 +0200

    Add project files.

---
+++ Messages/MessageTest1.cs
@@ -0,0 +1,12 @@
+﻿using ProtoBuf;
+
+[ProtoContract]
+public class MessageTest1 : BaseMessage
+{
+    [ProtoMember(1)]
+    int _data { get; set; }
+    public override void Process(Server.BaseConnection connection)
+    {
+        System.Console.WriteLine(">>>> SerializableMessage1");
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Threading.Tasks;
+using ProtoBuf;
+
+namespace Server { 
+    class Program {
+        static void Main(string[] args) {
+            BaseMessage.Init();
+            BaseRoom lobby = new BaseRoom();
+            var sockServer = new TcpListener(System.Net.IPAddress.Parse("0.0.0.0"), 16384);
+            sockServer.Start();            
+            while (true) {
+                var tmp = new Server.BaseConnection();
+                tmp.Init(sockServer.AcceptTcpClient(), lobby);
+            }
+        }
+    }
+}
